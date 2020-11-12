40f1cfb52e65c25cd61df1486f3ea60ec4891440
ottory <56276237+ottory@users.noreply.github.com>
2020-11-12 08:36:58 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -43,3 +43,47 @@ function displayEmployees(employeeData) {
         });
         gridContainer.innerHTML = employeeHTML;
 }
+
+function displayModal(index) {
+
+    ////use object destructuring to make our template literal cleaner
+    let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
+
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+        <img class="avatar" srch="${picture.large}" />
+        <div class="text-container">
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        <hr />
+        <p>${phone}</p>
+        <p class="address">${street}, ${state} ${postcode}</p>
+        <p>Birthday:
+        ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+        `;
+
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
+
+gridContainer.addEventListener('click', e => {
+
+    ///make sure the click is not on the gridContainer itself
+    if (e.target !== gridContainer) {
+
+        ////select the card element based on its proximity to actual element clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+/// hidden class to the modal overlay 
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
